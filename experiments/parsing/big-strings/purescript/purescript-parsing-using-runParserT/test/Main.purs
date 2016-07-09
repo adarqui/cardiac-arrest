6660ed107b072a1d9e44c76d8a8e5b4976daed34
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-09 18:03:22 -0400

    whoa.. WORKS!! but slow af

--- experiments/parsing/big-strings/purescript/purescript-parsing-using-runParserT/test/Main.purs
+++ experiments/parsing/big-strings/purescript/purescript-parsing-using-runParserT/test/Main.purs
@@ -2,31 +2,109 @@ module Test.Main (
   main
 ) where
 
+import Control.Alt ((<|>))
+import Control.Applicative           ((*>))
 import Control.Monad.Aff             (makeAff)
 import Control.Monad.Eff             (Eff)
 import Control.Monad.Eff.Console     (CONSOLE)
-import Control.Monad.Trampoline      (runTrampoline)
-import Control.Applicative           ((*>))
+import Control.Monad.Trampoline      (Trampoline, runTrampoline)
 import Data.Array                    ((..))
 import Data.Either                   (Either(..))
 import Data.Identity (runIdentity)
 import Data.String                   as String
 import Data.Traversable              (for)
 import Data.Unfoldable               (replicate)
-import Prelude                       (Unit, unit, void, bind, show, ($), (*), (<>), (==), (<<<))
+import Prelude                       (class Monad, Unit, unit, void, bind, show, ($), (*), (<>), (==), (<<<))
 import Test.Unit                     (suite, test)
 import Test.Unit.Main                (runTest)
 import Test.Unit.Console             (TESTOUTPUT, print)
 import Test.Unit.Assert              as Assert
-import Text.Parsing.Parser           (PState(..), runParserT)
-import Text.Parsing.Parser.Language  (haskellDef)
-import Text.Parsing.Parser.Pos       (initialPos)
-import Text.Parsing.Parser.Token     (TokenParser, makeTokenParser)
+import Text.Parsing.Parser
+import Text.Parsing.Parser.Language
+import Text.Parsing.Parser.Pos
+import Text.Parsing.Parser.String
+import Text.Parsing.Parser.Token
 
 
 
-testTokenParser :: TokenParser
-testTokenParser = makeTokenParser haskellDef
+type TrampolineTokenParser = GenTokenParser String Trampoline
+type TrampolineLanguageDef = GenLanguageDef String Trampoline
+
+
+
+-- | The language definition for the Haskell language.
+trampolineHaskellDef  :: TrampolineLanguageDef
+trampolineHaskellDef   =
+    case trampolineHaskell98Def of
+        (LanguageDef def) -> LanguageDef def
+                { identLetter    = def.identLetter <|> char '#'
+                , reservedNames  = def.reservedNames <>
+                                   ["foreign","import","export","primitive"
+                                   ,"_ccall_","_casm_"
+                                   ,"forall"
+                                   ]
+                }
+
+
+
+-- | The language definition for the language Haskell98.
+trampolineHaskell98Def :: TrampolineLanguageDef
+trampolineHaskell98Def = LanguageDef (unGenLanguageDef trampolineHaskellStyle)
+                { reservedOpNames = ["::","..","=","\\","|","<-","->","@","~","=>"]
+                , reservedNames   = [ "let","in","case","of","if","then","else"
+                                    , "data","type"
+                                    , "class","default","deriving","do","import"
+                                    , "infix","infixl","infixr","instance","module"
+                                    , "newtype","where"
+                                    , "primitive"
+                                    -- "as","qualified","hiding"
+                                    ]
+                }
+
+
+
+trampolineHaskellStyle :: TrampolineLanguageDef
+trampolineHaskellStyle = LanguageDef (unGenLanguageDef trampolineEmptyDef)
+                { commentStart    = "{-"
+                , commentEnd      = "-}"
+                , commentLine     = "--"
+                , nestedComments  = true
+                , identStart      = letter
+                , identLetter     = alphaNum <|> oneOf ['_', '\'']
+                , opStart         = op'
+                , opLetter        = op'
+                , reservedOpNames = []
+                , reservedNames   = []
+                , caseSensitive   = true
+                }
+  where
+    op' :: forall m . (Monad m) => ParserT String m Char
+    op' = oneOf [':', '!', '#', '$', '%', '&', '*', '+', '.', '/', '<', '=', '>', '?', '@', '\\', '^', '|', '-', '~']
+
+
+
+trampolineEmptyDef :: TrampolineLanguageDef
+trampolineEmptyDef = LanguageDef
+            { commentStart:    ""
+            , commentEnd:      ""
+            , commentLine:     ""
+            , nestedComments:  true
+            , identStart:      letter <|> char '_'
+            , identLetter:     alphaNum <|> oneOf ['_', '\'']
+            , opStart:         op'
+            , opLetter:        op'
+            , reservedOpNames: []
+            , reservedNames:   []
+            , caseSensitive:   true
+            }
+  where
+    op' :: forall m . (Monad m) => ParserT String m Char
+    op' = oneOf [':', '!', '#', '$', '%', '&', '*', '+', '.', '/', '<', '=', '>', '?', '@', '\\', '^', '|', '-', '~']
+
+
+
+testTokenParser :: TrampolineTokenParser
+testTokenParser = makeTokenParser trampolineHaskellDef
 
 
 
